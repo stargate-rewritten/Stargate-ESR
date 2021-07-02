11de9232f6ff8275823c398f4aaebea13ac3d756
Ghost_chu <2908803755@qq.com>
2021-07-02 09:41:53 +0800

    Resolved #36

--- src/main/java/net/TheDgtl/Stargate/threads/BlockPopulatorThread.java
+++ src/main/java/net/TheDgtl/Stargate/threads/BlockPopulatorThread.java
@@ -30,6 +30,8 @@ public class BlockPopulatorThread extends StargateRunnable {
                 EndGateway gateway = (EndGateway) blk.getState();
                 gateway.setExitLocation(blk.getWorld().getSpawnLocation());
                 gateway.setExactTeleport(true);
+                // https://github.com/stargate-bukkit/Stargate-Bukkit/issues/36
+                gateway.setAge(-9223372036854775808L);
                 gateway.update(false, false);
             } else if (b.getAxis() != null) {
                 Orientable orientable = (Orientable) blk.getBlockData();
